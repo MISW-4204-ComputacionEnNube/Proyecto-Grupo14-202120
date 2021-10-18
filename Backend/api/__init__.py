0c20b76d0430ccb496acf06da062be34ffdb950e
José López <jl.lopez77@uniandes.edu.co>
2021-10-18 17:12:32 -0500

    Actualización conexión db y base

--- Backend/api/__init__.py
+++ Backend/api/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import json
 
 def create_app(config_name):
     app = Flask(__name__)
 
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/vynils'
+    with open('credenciales.json') as arch:
+        config = json.load(arch)
+    conexion = 'postgresql://' + config['user']+ ':' + config['password'] + '@' + config['host'] + ':' + str(config['port']) + '/' + config['database']
+    app.config['SQLALCHEMY_DATABASE_URI'] = conexion
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['JWT_SECRET_KEY']='Maestría-en-Ingeniería-de-Software-Miso2021'
