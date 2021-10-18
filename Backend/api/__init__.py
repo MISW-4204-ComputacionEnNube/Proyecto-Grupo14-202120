318fec51b5fa854579eac3b9ca58ae145750b2c9
Harold Funeme <h.funeme@uniandes.edu.co>
2021-10-18 18:28:52 -0500

    change schema to sqlite and add gitignore

--- Backend/api/__init__.py
+++ Backend/api/__init__.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 def create_app(config_name):
     app = Flask(__name__)
 
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/vynils'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/vynils'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['JWT_SECRET_KEY']='Maestría-en-Ingeniería-de-Software-Miso2021'
