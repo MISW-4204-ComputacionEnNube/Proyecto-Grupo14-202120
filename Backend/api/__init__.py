a415259b3c994f095edde8ac9c6fadd910ddda64
santiago alejandro salinas vargas <s.salinasv@uniandes.edu.co>
2021-10-20 16:20:15 -0500

    se agrega debug a True

--- Backend/api/__init__.py
+++ Backend/api/__init__.py
@@ -43,5 +43,6 @@ def create_app():
     app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fa'
     app.config['JWT_SECRET_KEY']='Maestría-en-Ingeniería-de-Software-Miso2021'
     app.config['PROPAGATE_EXCEPTIONS'] = True
+    app.config['DEBUG'] = True
 
     return app
