97d8f534a0f8cf98358182be98e0614db249cd3a
santiago alejandro salinas vargas <s.salinasv@uniandes.edu.co>
2021-10-21 19:56:16 -0500

    se ajusta la vista de tareas para que almacene el archivo enviado

--- Backend/api/vistas/vistas.py
+++ Backend/api/vistas/vistas.py
@@ -7,14 +7,16 @@
 
 # ----------------------------------------------------------------------------
 
-from operator import contains
 from flask import request
-from ..modelos import db, Usuario, UsuarioSchema, Tarea, TareaSchema
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required, create_access_token, \
+    get_jwt_identity
 from sqlalchemy.exc import IntegrityError
-from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 from datetime import datetime
+from operator import contains
+
 from ..tareas import registrar_log
+from ..modelos import db, Usuario, UsuarioSchema, Tarea, TareaSchema, Formato
 
 # ----------------------------------------------------------------------------
 
@@ -34,6 +36,9 @@ __date__ = "2021-10-19 15:46"
 
 usuario_schema = UsuarioSchema()
 tarea_schema = TareaSchema()
+ruta = "/home/estudiante/Proyecto-Grupo14-202120/Backend/files"
+formatos = ['acc', 'mp3', 'ogg', 'wav', 'wma']
+
 
 # end point: /api/tasks
 class VistaTareas(Resource):
@@ -42,10 +47,76 @@ class VistaTareas(Resource):
     def post(self):
         """Se crea una nueva tarea."""
 
-        nueva_tarea = Tarea(archivo = request.json["archivo"],
-            formato = request.json["formato"],
-            fecha = datetime.today(),
-            estado = 1,
+        # obtiene el archivo enviado
+        f = request.files['file']
+        # obtiene el tipo de archivo al que se transformará
+        extension_destino = request.json['destino']
+
+        if f is None:
+            # no se envio el archivo, por ende no se crea la tarea
+            return "No se cargo el archivo", 402        
+
+        if extension_destino is None:
+            # no se envio la extensión de destino, por ende no se crea la tarea
+            return "No se definió la extensión de destino", 402        
+
+        # obtiene el nombre del archivo
+        archivo = f.filename
+        # obtiene la extensión del archivo en minúscula
+        extension_origen = archivo.split('.')[-1].lower()
+        # obtiene la base del nombre del archivo
+        base_archivo = archivo[:(len(archivo)-len(extension_origen))]
+        # pasa la extensión de destino a minúscula
+        extension_destino = extension_destino.lower()
+
+        # valida que el nombre de archivo tenga base
+        if len(base_archivo) == 0:
+            # nombre de archivo sin base
+            return "Nombre de archivo sin base", 402
+
+        # valida la extensión del archivo de origen
+        if extension_origen in formatos:
+            # el formato del archivo no es admitido
+            return "El formato del archivo no es admitido", 402
+
+        # valida la extensión de destino
+        if extension_destino in formatos:
+            # el formato de destino no es admitido
+            return "El formato de destino no es admitido", 402
+
+        # obtiene la fecha actual
+        fecha = datetime.now()
+        tfecha = fecha.strftime('%Y%m%d%H%M%S')
+
+        # genera el nombre del archivo con el que se almacenará el archivo
+        archivo_origen = f"{tfecha}_{archivo}".replace(' ', '_')
+
+        # genera el nombre del archivo con el que se almacenará el archivo 
+        # transformado
+        archivo_destino = f"{tfecha}_{base_archivo}.{extension_destino}".\
+            replace(' ', '_')
+
+        try:
+            # almacena el archivo
+            f.save(archivo_origen)
+        except:
+            return "Error al almacenar el archivo", 402
+
+        # construye la ruta donde se almacenó el archivo
+        ruta_archivo_origen = f"{ruta}/{archivo_origen}"
+        # construye la ruta donde se almacenará el archivo transformado
+        ruta_archivo_destino = f"{ruta}/{archivo_destino}"
+
+
+        # crea el registro en la base de datos
+        nueva_tarea = Tarea(
+            archivo = archivo,
+            formato_origen = extension_origen,
+            ruta_archivo_origen = ruta_archivo_origen,
+            formato_destino = extension_destino,
+            ruta_archivo_destino = ruta_archivo_destino,
+            fecha = fecha,
+            estado = 'uploaded',
             usuario_id = request.json["usuario_id"])
 
         db.session.add(nueva_tarea)
